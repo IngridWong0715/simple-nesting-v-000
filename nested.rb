52176bfdb0b0a884a0b5584e90ba97c252801a89
Ingrid Wong <yw340@georgetown.edu>
2018-01-14 23:04:03 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -60,12 +60,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -105,7 +99,6 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 		 programmer_hash
@@ -129,5 +122,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 
 end
